db5864bf9901b4c5765934b8a7915331f1d66570
dsign12345 <90810626+dsign12345@users.noreply.github.com>
2021-09-17 09:47:22 +0800

    Update m-c.js

--- m-c.js
+++ m-c.js
@@ -6,5 +6,122 @@ if (typeof jQuery === "undefined") {
 }
 
 window.onload = function() {
-  $(function(){ alert("jQuery + DOM loaded."); });   
+  	jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-left').show();
+    jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-right').addClass('active');
+
+    jQuery('.defensemen-collection-list .defensemen-collection-list-item:first-child').find('.defensemen-statistics-content-left').show();
+    jQuery('.defensemen-collection-list .defensemen-collection-list-item:first-child').find('.defensemen-statistics-content-right').addClass('active');
+
+    jQuery('.rookies-collection-list .rookies-collection-list-item:first-child').find('.rookies-statistics-content-left').show();
+    jQuery('.rookies-collection-list .rookies-collection-list-item:first-child').find('.rookies-statistics-content-right').addClass('active');
+
+    jQuery('.goalies-collection-list .goalies-collection-list-item:first-child').find('.goalies-statistics-content-left').show();
+    jQuery('.goalies-collection-list .goalies-collection-list-item:first-child').find('.goalies-statistics-content-right').addClass('active');
+
+    if ( jQuery('.player-sort-button.sort-active').find('.sort-button-text').text() == 'POINTS' ) {
+	    
+    	jQuery('.players-collection-list').find('.player-goals').hide();
+      	jQuery('.players-collection-list').find('.player-assists').hide();
+    
+    }
+    
+    if ( jQuery('.defensemen-sort-button.sort-active').find('.sort-button-text').text() == 'POINTS' ) {
+    
+	jQuery('.defensemen-collection-list').find('.player-goals').hide();
+	jQuery('.defensemen-collection-list').find('.player-assists').hide();
+    
+    }
+
+    if ( jQuery('.rookies-sort-button.sort-active').find('.sort-button-text').text() == 'POINTS' ) {
+    
+	jQuery('.rookies-collection-list').find('.player-goals').hide();
+	jQuery('.rookies-collection-list').find('.player-assists').hide();
+    
+    }
+    
+    if ( jQuery('.goalies-sort-button.sort-active').find('.sort-button-text').text() == 'GAA' ) {
+    
+	jQuery('.goalies-collection-list').find('.player-goals').hide();
+	jQuery('.goalies-collection-list').find('.player-assists').hide();
+    
+    }
+    
+    jQuery('#players-filter .player-sort-button').each(function() {
+	    
+		jQuery(this).click(function() {
+                
+	    var clickedButton = jQuery(this).find('.sort-button-text').text();
+            
+            if ( clickedButton == 'POINTS' ) {
+
+                jQuery('.players-collection-list').find('.player-points').show();
+                jQuery('.players-collection-list').find('.player-goals').hide();
+                jQuery('.players-collection-list').find('.player-assists').hide();
+            
+
+                jQuery('.players-collection-list .players-collection-list-item').each(function() {
+                    jQuery(this).find('.player-statistics-content-left').hide();
+                    jQuery(this).find('.player-statistics-content-right').removeClass('active');
+                });
+
+                jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-left').show();
+                jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-right').addClass('active');
+            
+            } else if ( clickedButton == 'GOALS' ) {
+        
+                jQuery('.players-collection-list').find('.player-points').hide();
+                jQuery('.players-collection-list').find('.player-goals').show();
+                jQuery('.players-collection-list').find('.player-assists').hide();
+                
+
+                jQuery('.players-collection-list .players-collection-list-item ').each(function() {
+                    jQuery(this).find('.player-statistics-content-left').hide();
+                    jQuery(this).find('.player-statistics-content-right').removeClass('active');
+                });
+
+                jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-left').show();
+                jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-right').addClass('active');
+                
+            } else if ( clickedButton == 'ASSISTS' ) {    
+
+                jQuery('.players-collection-list').find('.player-points').hide();
+                jQuery('.players-collection-list').find('.player-goals').hide();
+                jQuery('.players-collection-list').find('.player-assists').show();
+                
+
+                jQuery('.players-collection-list .players-collection-list-item').each(function() {
+                    jQuery(this).find('.player-statistics-content-left').hide();
+                    jQuery(this).find('.player-statistics-content-right').removeClass('active');
+                });
+
+                jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-left').show();
+                jQuery('.players-collection-list .players-collection-list-item:first-child').find('.player-statistics-content-right').addClass('active');
+            
+            }
+    
+      });
+    });
+    
+    jQuery('.player-statistics-content-right').each(function() {
+      
+        jQuery(this).click(function() {
+              
+          if ( !jQuery(this).hasClass('active') ) {
+              
+            jQuery('.player-statistics-content-right').each(function() {
+                jQuery(this).removeClass('active');
+            });
+            
+            jQuery('.player-statistics-content-left').each(function() {
+                jQuery(this).hide();
+            });
+            
+            jQuery(this).prev().show();
+            jQuery(this).addClass('active');
+          
+          } 
+       
+      });
+      
+    }); 
 }
